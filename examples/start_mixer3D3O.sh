fc9d776911b695af6256d4f62bf8929e15c5d246
ritsch <ritsch@algo.mur.at>
2017-05-18 14:37:19 +0200

    new states

--- examples/start_mixer3D3O.sh
+++ examples/start_mixer3D3O.sh
@@ -2,8 +2,17 @@
 calldir=$(pwd)
 cd $(dirname $0)
 
+if [ -x ../../../libs/pd/bin/pd ]; then
+	PD=../../../libs/pd/bin/pd
+elif [ -x ../../../../libs/pd/bin/pd ]; then
+	PD=../../../../libs/pd/bin/pd
+else
+	PD=pd
+fi
+
+echo using PD=$PD
 
 # start pd with 18 channels out: 1,2= Stereo, 3-18 Ambisonics BUS, 19=SUB, 20=monitoring
-pd -jack -jackname Pd-mixer3D3O -inchannels 2 -outchannels 20  mixer3D3O.pd
+$PD -jack -jackname Pd-mixer3D3O -inchannels 2 -outchannels 20  mixer3D3O.pd
 
 cd $calldir
