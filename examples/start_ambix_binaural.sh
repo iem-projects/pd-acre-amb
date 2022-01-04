f80d9e470e45ba44f3623e8513702225843e417e
winfried <winfried@koko.pichla>
2022-01-04 23:59:56 +0100

    updated scripts

--- examples/start_ambix_binaural.sh
+++ examples/start_ambix_binaural.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 #
-# start a binaural decoder from ambix as standalone and patch it to 
-# to a Ambisonics-Bus from PD eg. mixer3D3O
+# start a binaural decoder from ambix toolbox as standalone 
+# and patch it to a Ambisonics-Bus from PD eg. mixer3D3O
 #
 # if not installed by system, see https://github.com/kronihias/ambix
 # 
 cd $(dirname $0)
+AMBIX_BINAURAL=$(which ambix_binaural_standalone_o5)
 
-ambix_binaural_standalone_o5 ../data/binaural/binaural_o5_setup.state
+if [ "x${AMBIX_BINAURAL}" == "x" ]
+then
+    echo no ambix_binaural standalone application found
+    echo .. see https://github.com/kronihias/ambix for installation
+    exit 1
+else
+    echo found $AMBIX_BINAURAL
+fi
+
+$AMBIX_BINAURAL ../data/binaural/binaural_o5_setup.state
--- examples/start_mixer3D3O.sh
+++ examples/start_mixer3D3O.sh
@@ -2,14 +2,7 @@
 calldir=$(pwd)
 cd $(dirname $0)
 
-if [ -x ../../../libs/pd/bin/pd ]; then
-	PD=../../../libs/pd/bin/pd
-elif [ -x ../../../../libs/pd/bin/pd ]; then
-	PD=../../../../libs/pd/bin/pd
-else
-	PD=pd
-fi
-
+PD=$(which pd)
 echo using PD=$PD
 
 # 18 channels in: 1,2 stereo in, 3-18 Ambisonics In see patch
@@ -17,13 +10,10 @@ INCHANNELS=18
 # 20 channels out: 1,2= Stereo, 3-18 Ambisonics BUS, 19=SUB, 20=monitoring
 OUTCHANNELS=20
 
-
 $PD -jack -jackname Pd-mixer3D3O -inchannels $INCHANNELS -outchannels $OUTCHANNELS  mixer3D3O.pd
 
 if [ $? -ne 0 ]; then
 	echo try without jackname
 	$PD -jack -inchannels $INCHANNELS -outchannels $OUTCHANNELS  mixer3D3O.pd
 fi
-
-
 cd $calldir
